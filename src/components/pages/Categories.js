c16ba0b6c258c139173f43c4327c0e1e7fdef4d7
AtoBrightSide <meseleabel99@gmail.com>
2021-10-13 00:35:57 +0300

    Managed to fetch category, area from api

--- src/components/pages/Categories.js
+++ src/components/pages/Categories.js
@@ -1,31 +1,37 @@
 import '../../App.css';
-import axios from 'axios';
-import { useState, React, useEffect } from 'react';
+import Cards from '../Cards';
 
-const baseURL = 'https://themealdb.com/api/json/v1/1/list.php?c=list';
 
 export default function Categories() {
-    const [categories, setCategories] = useState(null);
 
-    useEffect(() => {
-        axios.get(baseURL).then((response) => {
-            setCategories(response.data);
-            // console.log(response.data.meals);
-        })
-    }, []);
-    if (categories) {
-        return (
-            <div>
-                <h1 className="categories">Categories</h1>
-                {categories.meals.map(cat => {
-                    return <h1>{cat.strCategory}</h1>
-                })}
-            </div>
-        );
-    }
+    // const [categories, setCategories] = useState(null);
+
+    // useEffect(() => {
+    //     axios.get(baseURL).then((response) => {
+    //         setCategories(response.data);
+    //     })
+    // }, []);
+    // if (categories) {
+    //     return (
+    //         <div>
+    //             <h1 className="categories">Categories</h1>
+    //             {categories.meals.map(cat => {
+    //                 return <CardItem 
+    //                     label={cat.strCategory}
+    //                     title="This is what it is all about yeah!!"
+    //                     path=""
+    //                     src="https://picsum.photos/200/300"
+    //                 />
+    //             })}
+    //         </div>
+    //     );
+    // }
 
     return (
         <>
+            <Cards 
+                letter='c'
+            />
         </>
     );
 }
--- src/components/pages/Countries.js
+++ src/components/pages/Countries.js
@@ -1,30 +1,14 @@
 import '../../App.css';
-import axios from 'axios';
-import { useState, React, useEffect } from 'react';
+import { React } from 'react';
+import Cards from '../Cards';
 
-const baseURL = 'https://themealdb.com/api/json/v1/1/list.php?a=list';
 
 export default function Countries() {
-    const [countries, setCountries] = useState(null);
-
-    useEffect(() => {
-        axios.get(baseURL).then((response) => {
-            setCountries(response.data);
-        })
-    }, []);
-    if (countries) {
-        return (
-            <div>
-                <h1 className="Countries">Countries</h1>
-                {countries.meals.map(cat => {
-                    return <h1>{cat.strArea}</h1>
-                })}
-            </div>
-        );
-    }
-
     return (
         <>
+            <Cards
+                letter='a'
+            />
         </>
     );
 }
