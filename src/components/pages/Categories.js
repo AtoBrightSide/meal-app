853305dce69aac89fc3e7d190b11db2ad0695958
AtoBrightSide <meseleabel99@gmail.com>
2021-10-12 23:36:25 +0300

    Started fetching the data

--- src/components/pages/Categories.js
+++ src/components/pages/Categories.js
@@ -1,9 +1,31 @@
-import React from 'react'
 import '../../App.css';
+import axios from 'axios';
+import { useState, React, useEffect } from 'react';
 
+const baseURL = 'https://themealdb.com/api/json/v1/1/list.php?c=list';
 
 export default function Categories() {
-    return(
-        <h1 className="categories">Categories</h1>
+    const [categories, setCategories] = useState(null);
+
+    useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setCategories(response.data);
+            // console.log(response.data.meals);
+        })
+    }, []);
+    if (categories) {
+        return (
+            <div>
+                <h1 className="categories">Categories</h1>
+                {categories.meals.map(cat => {
+                    return <h1>{cat.strCategory}</h1>
+                })}
+            </div>
+        );
+    }
+
+    return (
+        <>
+        </>
     );
 }
--- src/components/pages/Countries.js
+++ src/components/pages/Countries.js
@@ -1,9 +1,30 @@
-import React from 'react'
 import '../../App.css';
+import axios from 'axios';
+import { useState, React, useEffect } from 'react';
 
+const baseURL = 'https://themealdb.com/api/json/v1/1/list.php?a=list';
 
 export default function Countries() {
-    return(
-        <h1 className="countries">Countries</h1>
+    const [countries, setCountries] = useState(null);
+
+    useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setCountries(response.data);
+        })
+    }, []);
+    if (countries) {
+        return (
+            <div>
+                <h1 className="Countries">Countries</h1>
+                {countries.meals.map(cat => {
+                    return <h1>{cat.strArea}</h1>
+                })}
+            </div>
+        );
+    }
+
+    return (
+        <>
+        </>
     );
 }
