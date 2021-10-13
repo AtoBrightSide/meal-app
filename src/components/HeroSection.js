7a0811b9f05bad5bb47aac95576f30fb15b704c4
AtoBrightSide <meseleabel99@gmail.com>
2021-10-13 18:20:51 +0300

    made the herosection fetch random background image

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,32 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from './Button'
 import '../App.css';
 import './Herosection.css';
+import axios from 'axios';
 
 function HeroSection() {
-    return (
-        <div className='hero-container'>
-            <h1>FOOD AWAITS</h1>
-            <p>What are you waiting for?</p>
-            <div className="hero-btns">
-                <Button
-                    className='btns'
-                    buttonStyle='btn--outline'
-                    buttonSize='btn--large'
-                >
-                    GET STARTED
-                </Button>
-                <Button
-                    className='btns'
-                    buttonStyle='btn--primary'
-                    buttonSize='btn--large'
-                >
-                    WATCH TRAILER <i className="far fa-play-circle"></i>
-                </Button>
-                
+    const baseURL = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setData(response.data);
+        })
+    }, [baseURL]);
+
+
+    if (data) {
+        const myStyle = {
+            background: `url(${data.meals[0].strMealThumb}) center center/cover no-repeat`,
+        };
+
+        return (
+            <div className='hero-container' style={myStyle}>
+                <h1>FOOD AWAITS</h1>
+                <p>What are you waiting for?</p>
+                <div className="hero-btns">
+                    <Button
+                        className='btns'
+                        buttonStyle='btn--outline'
+                        buttonSize='btn--large'
+                    >
+                        GET STARTED
+                    </Button>
+                    <Button
+                        className='btns'
+                        buttonStyle='btn--primary'
+                        buttonSize='btn--large'
+                    >
+                        WATCH TRAILER <i className="far fa-play-circle"></i>
+                    </Button>
+
+                </div>
             </div>
-        </div>
-    )
+        );
+    } else {
+        return (
+            <></>
+        );
+    }
 }
 
 export default HeroSection
