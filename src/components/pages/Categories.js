edb77e25888c67ec735f7bb8c6ed5b6db7f8d2f4
AtoBrightSide <meseleabel99@gmail.com>
2021-10-13 17:18:38 +0300

    updated the categories page

--- src/components/pages/Categories.js
+++ src/components/pages/Categories.js
@@ -3,30 +3,6 @@ import Cards from '../Cards';
 
 
 export default function Categories() {
-
-    // const [categories, setCategories] = useState(null);
-
-    // useEffect(() => {
-    //     axios.get(baseURL).then((response) => {
-    //         setCategories(response.data);
-    //     })
-    // }, []);
-    // if (categories) {
-    //     return (
-    //         <div>
-    //             <h1 className="categories">Categories</h1>
-    //             {categories.meals.map(cat => {
-    //                 return <CardItem 
-    //                     label={cat.strCategory}
-    //                     title="This is what it is all about yeah!!"
-    //                     path=""
-    //                     src="https://picsum.photos/200/300"
-    //                 />
-    //             })}
-    //         </div>
-    //     );
-    // }
-
     return (
         <>
             <Cards 
